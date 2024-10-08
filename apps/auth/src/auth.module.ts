9f165264fd24b66554cb622e1c89368c30e3aa26
QPM <admin@QuantumPinnacleMatrix.tech>
2024-10-08 09:55:22 +0300

    [ADD:] OpenAPI , init auth service

---
+++ apps/auth/src/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().port().default(4000),
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
---
+++ apps/auth/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthModule } from './auth.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AuthModule);
+  const configService = app.get(ConfigService);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Auth Service')
+    .setDescription('Auth API')
+    .setVersion('1.0')
+    .build();
+  const doc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, doc);
+  console.log(`Listening on port ${configService.get('PORT')}`);
+  await app.listen(configService.get('PORT'));
+}
+bootstrap();
